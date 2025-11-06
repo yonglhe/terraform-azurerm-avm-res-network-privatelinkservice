e1d875585e7ba5f12bc3f2069f00a74f2e41d317
yonglhe <yg.he@outlook.com>
2025-11-06 16:59:55 +0100

    resolved frontend ip of lb

--- tests/main.tf
+++ tests/main.tf
@@ -18,68 +18,72 @@ provider "azurerm" {
   }
 }
 
-resource "azurerm_resource_group" "test" {
+resource "azurerm_resource_group" "this" {
   name     = "${var.name_prefix}-rg"
   location = var.location
 }
 
-resource "azurerm_virtual_network" "test" {
+resource "azurerm_virtual_network" "this" {
   name                = "${var.name_prefix}-vnet"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.test.location
-  resource_group_name = azurerm_resource_group.test.name
 }
 
-resource "azurerm_subnet" "test" {
+resource "azurerm_subnet" "pls_subnet" {
   name                 = "${var.name_prefix}-subnet"
-  resource_group_name  = azurerm_resource_group.test.name
-  virtual_network_name = azurerm_virtual_network.test.name
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
   address_prefixes     = ["10.0.3.0/24"]
 
   private_link_service_network_policies_enabled = false
 }
 
-resource "azurerm_public_ip" "test" {
-  name                = "${var.name_prefix}-public-ip"
-  location            = azurerm_resource_group.test.location
-  resource_group_name = azurerm_resource_group.test.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
+resource "azurerm_subnet" "lb_subnet" {
+  name                 = "${var.name_prefix}-lb-subnet"
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = ["10.0.2.0/24"]
+
+  private_link_service_network_policies_enabled = false
 }
 
-resource "azurerm_lb" "test" {
-  name                = "${var.name_prefix}-lb-subnet"
-  location            = azurerm_resource_group.test.location
-  resource_group_name = azurerm_resource_group.test.name
+resource "azurerm_lb" "this" {
+  name                = "${var.name_prefix}-lb"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
   sku                 = "Standard"
 
   frontend_ip_configuration {
     name                 = "${var.name_prefix}-lb-frontend-ip"
-    public_ip_address_id = azurerm_public_ip.test.id
+    subnet_id                     = azurerm_subnet.lb_subnet.id
+    private_ip_address_allocation = "Dynamic"
   }
 }
 
-
 module "azurerm_private_link_service" {
-  source = "/mnt/c/Users/yhe/terraform-azure/core/core-terraform-azurerm-avm-res-network-privatelinkservice/"  # path to your AVM module
+  source = "/mnt/c/Users/yhe/terraform-azure/core/core-terraform-azurerm-avm-res-network-privatelinkservice/"
 
   name                  = "${var.name_prefix}-private-link-service"
-  resource_group_name   = azurerm_resource_group.test.name
-  location              = azurerm_resource_group.test.location
-  enable_proxy_protocol = false
+  resource_group_name   = azurerm_resource_group.this.name
+  location              = azurerm_resource_group.this.location
+  # enable_proxy_protocol = false
 
-  load_balancer_frontend_ip_configuration_ids = [azurerm_lb.test.frontend_ip_configuration[0].id]
+  load_balancer_frontend_ip_configuration_ids = [
+    azurerm_lb.this.frontend_ip_configuration[0].id
+    ]
+
 
   nat_ip_configurations = [
     {
       name                       = "primary"
-      subnet_id                  = azurerm_subnet.test.id
+      subnet_id                  = azurerm_subnet.pls_subnet.id
       primary                    = true
       private_ip_address_version = "IPv4"
+      private_ip_address         = "10.0.3.4"
     }
   ]
 }
-
 
 # Access outputs
 output "pls_resource_name" {
