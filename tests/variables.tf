eecfa83228f6152fc561f270e9612dc69e8663fb
yonglhe <yg.he@outlook.com>
2025-11-28 10:34:49 +0100

    removed unnecessary, updated readme

--- tests/variables.tf
+++ tests/variables.tf
@@ -1,7 +1,7 @@
 variable "name_prefix" {
   description = "Prefix for resource names"
   type        = string
-  default     = "dev-avm-pls-test"
+  default     = "avm-pls-test"
 }
 
 variable "location" {
